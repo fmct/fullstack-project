ffb1bcda07ecd28e6390ddd028e2a4fd77c347b4
fmct <filipetavares@ua.pt>
2021-05-18 12:24:08 +0100

    name of web app changed

---
+++ backend/src/test/java/application/controller/impl/ProductControllerTest.java
@@ -0,0 +1,38 @@
+package application.controller.impl;
+
+
+import application.Application;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest(
+        SpringBootTest.WebEnvironment.MOCK,
+        classes = Application.class)
+@AutoConfigureMockMvc
+@TestPropertySource(
+        locations = "classpath:application-integrationtest.properties")
+public class ProductControllerTest {
+
+    @Autowired
+    private ProductController controller;
+
+    @Test
+    public void givenEmployees_whenGetEmployees_thenStatus200()
+            throws Exception {
+
+        getAllProducts("bob");
+
+        mvc.perform(get("/api/employees")
+                .contentType(MediaType.APPLICATION_JSON))
+                .andExpect(status().isOk())
+                .andExpect(content()
+                        .contentTypeCompatibleWith(MediaType.APPLICATION_JSON))
+                .andExpect(jsonPath("$[0].name", is("bob")));
+    }
+}
